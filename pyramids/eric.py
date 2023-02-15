c4dd1edcd7feccdc88f356783e5ca56e9f5d5bd6
Eric <enjui.chang@gmail.com>
2023-02-15 13:12:33 +0800

    Add Eric's solution

---
+++ pyramids/eric.py
@@ -0,0 +1,92 @@
+# Eric
+
+
+def pyramid_1(leng):
+    for i in range(leng):
+        string = ""
+        for j in range(leng-i):
+            if j == 0: 
+                string += "*"
+            else: 
+                string += " *" 
+        print(string)
+
+def pyramid_2(leng):
+    for i in range(leng):
+        string = ""
+        left_space = leng-i-1
+        total_length = leng + i
+
+        for j in range(left_space):
+            string += "  "
+        for j in range(total_length - left_space):
+            string += "* "
+        
+        print(string)
+
+def pyramid_3(leng):
+    for i in range(leng):
+        string = ""
+        left_space = leng- i - 1
+        total_length = leng + i + 1
+        
+        for j in range(left_space):
+            string += "  "
+        for j in range(total_length - left_space):
+            string += "* "
+        
+        print(string)
+
+def pyramid_4(leng):
+    for i in range(leng):
+        string = ""
+        left_space = leng-i-1
+        total_length = leng + i
+
+        for j in range(left_space):
+            string += "  "
+        for j in range(total_length - left_space):
+            string += "* "
+            
+        print(string)
+
+    for i in range(leng-1):
+        string = ""
+        left_space = i + 1
+        total_length = (leng-1) * 2 - i
+
+        for j in range(left_space):
+            string += "  "
+        for j in range(total_length - left_space):
+            string += "* "
+            
+        print(string)
+
+def pyramid_5(leng):
+    for i in range(leng):
+        string = ""
+        left_space = leng-i-1
+        total_length = leng + i
+
+        for j in range(left_space):
+            string += "  "
+        for j in range(total_length - left_space):
+            if j == 0: string += "* "
+            elif j == total_length - left_space-1: string += "*"
+            else: string += "  "
+            
+        print(string)
+
+    for i in range(leng-1):
+        string = ""
+        left_space = i + 1
+        total_length = (leng-1) * 2 - i
+
+        for j in range(left_space):
+            string += "  "
+        for j in range(total_length - left_space):
+            if j == 0: string += "* "
+            elif j == total_length - left_space-1: string += "*"
+            else: string += "  "
+            
+        print(string)
